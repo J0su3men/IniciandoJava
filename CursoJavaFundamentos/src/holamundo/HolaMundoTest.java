b76d5c572ec7437cf77997858e318c8236118a4b
J0su3:) <JOSUE@JOSUE-PC>
2017-10-18 14:09:19 -0500

    Signed-off-by: J0su3:) <JOSUE@JOSUE>

---
+++ CursoJavaFundamentos/src/holamundo/HolaMundoTest.java
@@ -0,0 +1,10 @@
+package holamundo;
+
+public class HolaMundoTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, Word con Java!!");//Imprime un saludo
+	}
+
+}
